6bd652161852b42fa2fffda6a59d98321ebd6fe7
cletus <cletushunsu@gmail.com>
2024-12-26 10:18:11 +0100

    updated file

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -7,6 +7,6 @@ namespace CrudApi.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        public DbSet<Items> Items { get; set; }
+        public DbSet<Item> Items { get; set; }
     }
 }
