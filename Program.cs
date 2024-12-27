66c32cc263be63d550992a309b3173a809fbdbc2
cletus <cletushunsu@gmail.com>
2024-12-27 13:53:03 +0100

    updated connection string

--- Program.cs
+++ Program.cs
@@ -4,12 +4,22 @@ using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Load environment variables from the .env file
+Env.Load();
+
 // Add services to the container.
 builder.Services.AddOpenApi();
 builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+connectionString = connectionString
+    .Replace("${DB_HOST}", Environment.GetEnvironmentVariable("DB_HOST"))
+    .Replace("${DB_PORT}", Environment.GetEnvironmentVariable("DB_PORT"))
+    .Replace("${DB_NAME}", Environment.GetEnvironmentVariable("DB_NAME"))
+    .Replace("${DB_USER}", Environment.GetEnvironmentVariable("DB_USER"))
+    .Replace("${DB_PASSWORD}", Environment.GetEnvironmentVariable("DB_PASSWORD"));
 
 // configure Postgresql
 builder.Services.AddDbContext<AppDbContext>(options => 
